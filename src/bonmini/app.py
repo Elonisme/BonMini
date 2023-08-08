bbb5756af01889c549689a328e763dd6b2108a05
elonisme <elonisme@163.com>
2023-08-08 10:41:00 +0800

    public v0.0.2

--- src/bonmini/app.py
+++ src/bonmini/app.py
@@ -3,10 +3,20 @@ This app is created for mini language translation to Chinese.
 """
 import toga
 import re
-import os
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 from pathlib import Path
+
+def is_chinese_sentence(sentence):
+    # 使用正则表达式检查句子中是否包含中文字符
+    chinese_pattern = re.compile(r'[\u4e00-\u9fa5]')
+    return bool(chinese_pattern.search(sentence))
+
+def is_english_sentence(sentence):
+    # 使用正则表达式检查句子是否只包含英文字母和标点符号
+    english_pattern = re.compile(r'^[A-Za-z\s.,!?;]+$')
+    return bool(english_pattern.match(sentence))
+
 
 def mini_init(): 
     resources_folder = Path(__file__).joinpath("../resources").resolve()
@@ -16,18 +26,38 @@ def mini_init():
         dictionary = {each.split("\t")[0]:each.split("\t")[1] for each in dictionary}
     return dictionary
 
-def mini2cn(mini, dictionary):
-    mini = re.split(' ',mini)
-    han = ""
-    for word in mini:
-        dic = dictionary.get(word)
-        if dic != None:
-            han += dic
-        else:
-            print(word+"单词拼写错误")
-            han += "x"
-            continue
-    return han
+def mini2cn(sentence, dictionary):
+    if is_english_sentence(sentence):
+        sentence = sentence.lower()
+        sentence = re.findall(r"[\w']+|[.,!?;]", sentence)
+        han = ""
+        for word in sentence:
+            dic = dictionary.get(word)
+            if dic != None:
+                han += dic
+            else:
+                print(word+"单词拼写错误")
+                han += "X"
+                continue
+        return han
+    elif is_chinese_sentence(sentence):
+        chinese_pattern = re.compile(r'[\u4e00-\u9fa5，。；？！]')
+        sentence = chinese_pattern.findall(sentence)
+        inverted_dict = {v: k for k, v in dictionary.items()}
+        mini = ""
+        for word in sentence:
+            dic = inverted_dict.get(word)
+            if dic != None:
+                mini += dic
+                mini += " "
+            else:
+                print(word+"单词拼写错误")
+                mini += "误"
+                continue
+        return mini
+    else:
+        return "请输入微语或在转义汉语"
+        
 
 
 class BonMini(toga.App):
@@ -35,7 +65,7 @@ class BonMini(toga.App):
         main_box = toga.Box(style=Pack(direction=COLUMN))
 
         name_label = toga.Label(
-            "微语: ",
+            "微语or转义汉字: ",
             style=Pack(padding=(0, 5))
         )
         self.name_input = toga.TextInput(style=Pack(flex=1))
@@ -45,7 +75,7 @@ class BonMini(toga.App):
         name_box.add(self.name_input)
 
         button = toga.Button(
-            "翻译",
+            "转写",
             on_press=self.say_hello,
             style=Pack(padding=5)
         )
@@ -62,12 +92,12 @@ class BonMini(toga.App):
         dict = mini_init()
         han = mini2cn(mini, dict)
         self.main_window.info_dialog(
-            f"微语: {self.name_input.value}",
-            "汉语: "+han
+            f"转义前: {self.name_input.value}",
+            "转义后: "+han
         )
 
 
 
 def main():
-    return BonMini()
+    return BonMini(app_id="0.0.2", app_name="BonMini")
 
